4df1ab3bca77451697cf9147b76dd69b0a36561f
Barin Britva <barinbritva@outlook.com>
2023-04-05 16:59:02 +0700

    Test coverage.

--- tests/event.spec.ts
+++ tests/event.spec.ts
@@ -2,10 +2,139 @@ import {describe, expect, it} from 'vitest';
 import {Event} from '../src';
 
 describe('class Event', () => {
-	it('should contain base data', () => {
+	it('should throw dates error', () => {
+		expect(() => {
+			new Event(
+				'Meet with friends',
+				new Date(Date.UTC(2021, 5, 18, 15, 0)),
+				new Date(Date.UTC(2021, 5, 18, 15, 0))
+			);
+		}).toThrow(
+			'End date must be greater than start date. Passed: start date - 2021-06-18T15:00:00.000Z, end date - 2021-06-18T15:00:00.000Z.'
+		);
+
+		expect(() => {
+			new Event(
+				'Meet with friends',
+				new Date(Date.UTC(2021, 5, 18, 15, 5)),
+				new Date(Date.UTC(2021, 5, 18, 15, 0))
+			);
+		}).toThrow(
+			'End date must be greater than start date. Passed: start date - 2021-06-18T15:05:00.000Z, end date - 2021-06-18T15:00:00.000Z.'
+		);
+	});
+
+	it('should do base features', () => {
 		const event = new Event('Meet with friends', new Date(Date.UTC(2021, 5, 18, 15, 0)));
 
 		expect(event.title).toEqual('Meet with friends');
+		expect(event.description).toEqual(undefined);
+		expect(event.location).toEqual(undefined);
 		expect(event.startDate.toISOString()).toEqual('2021-06-18T15:00:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-19T15:00:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-18');
+		expect(event.getEndDateAsString()).toEqual('2021-06-19');
+		expect(event.isAllDayEvent()).toEqual(true);
+		expect(event.hasAttendees()).toEqual(false);
+		expect(event.attendees.length).toEqual(0);
+		expect(event.attendees).toEqual([]);
+
+		event.changeTitle('Meet with friends and family');
+		expect(event.title).toEqual('Meet with friends and family');
+
+		event.changeDescription("Let's get together and grab some coffee!");
+		expect(event.description).toEqual("Let's get together and grab some coffee!");
+
+		event.changeLocation('Blue Bottle Coffee, 300 S Broadway, Los Angeles');
+		expect(event.location).toEqual('Blue Bottle Coffee, 300 S Broadway, Los Angeles');
+
+		event.addAttendees(['jane@example.com', 'Jane'], ['fred@example.com'], 'joe@example.com');
+		expect(event.hasAttendees()).toEqual(true);
+		expect(event.attendees.length).toEqual(3);
+		expect(event.attendees).toEqual([
+			['jane@example.com', 'Jane'],
+			['fred@example.com'],
+			'joe@example.com'
+		]);
+
+		event.clearAttendees();
+		expect(event.hasAttendees()).toEqual(false);
+		expect(event.attendees.length).toEqual(0);
+		expect(event.attendees).toEqual([]);
+	});
+
+	it('should reschedule start date', () => {
+		const event = new Event('Meet with friends', new Date(Date.UTC(2021, 5, 18, 15, 0)));
+		event.reschedule(new Date(Date.UTC(2021, 5, 19, 15, 0)));
+
+		expect(event.startDate.toISOString()).toEqual('2021-06-19T15:00:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-20T15:00:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-19');
+		expect(event.getEndDateAsString()).toEqual('2021-06-20');
+		expect(event.isAllDayEvent()).toEqual(true);
+	});
+
+	it('should set end date', () => {
+		const event = new Event('Meet with friends', new Date(Date.UTC(2021, 5, 18, 15, 0)));
+		event.reschedule(event.startDate, new Date(Date.UTC(2021, 5, 18, 17, 0)));
+
+		expect(event.startDate.toISOString()).toEqual('2021-06-18T15:00:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-18T17:00:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-18T15:00:00Z');
+		expect(event.getEndDateAsString()).toEqual('2021-06-18T17:00:00Z');
+		expect(event.isAllDayEvent()).toEqual(false);
+	});
+
+	it('should reschedule end date', () => {
+		const event = new Event(
+			'Meet with friends',
+			new Date(Date.UTC(2021, 5, 18, 15, 0)),
+			new Date(Date.UTC(2021, 5, 18, 17, 0))
+		);
+		event.reschedule(event.startDate, new Date(Date.UTC(2021, 5, 18, 17, 30)));
+
+		expect(event.startDate.toISOString()).toEqual('2021-06-18T15:00:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-18T17:30:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-18T15:00:00Z');
+		expect(event.getEndDateAsString()).toEqual('2021-06-18T17:30:00Z');
+	});
+
+	it('should reschedule end date through duration change', () => {
+		const event = new Event(
+			'Meet with friends',
+			new Date(Date.UTC(2021, 5, 18, 15, 0)),
+			new Date(Date.UTC(2021, 5, 18, 17, 0))
+		);
+		event.reschedule(event.startDate, 150);
+
+		expect(event.startDate.toISOString()).toEqual('2021-06-18T15:00:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-18T17:30:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-18T15:00:00Z');
+		expect(event.getEndDateAsString()).toEqual('2021-06-18T17:30:00Z');
+	});
+
+	it('should reschedule start date w/ keeping duration', () => {
+		const event = new Event('Meet with friends', new Date(Date.UTC(2021, 5, 18, 15, 0)), 150);
+		event.reschedule(new Date(Date.UTC(2021, 5, 19, 17, 20)));
+
+		expect(event.startDate.toISOString()).toEqual('2021-06-19T17:20:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-19T19:50:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-19T17:20:00Z');
+		expect(event.getEndDateAsString()).toEqual('2021-06-19T19:50:00Z');
+	});
+
+	it('should unset end date', () => {
+		const event = new Event(
+			'Meet with friends',
+			new Date(Date.UTC(2021, 5, 18, 15, 0)),
+			new Date(Date.UTC(2021, 5, 18, 17, 0))
+		);
+		event.reschedule(event.startDate, null);
+
+		expect(event.startDate.toISOString()).toEqual('2021-06-18T15:00:00.000Z');
+		expect(event.endDate.toISOString()).toEqual('2021-06-19T15:00:00.000Z');
+		expect(event.getStartDateAsString()).toEqual('2021-06-18');
+		expect(event.getEndDateAsString()).toEqual('2021-06-19');
+		expect(event.isAllDayEvent()).toEqual(true);
 	});
 });
--- tests/multi-generator.spec.ts
+++ tests/multi-generator.spec.ts
@@ -2,17 +2,6 @@ import {describe, expect, it} from 'vitest';
 import {Event, Google, ICalendar, MultiGenerator, Office365, Outlook, Yahoo} from '../src';
 
 describe('class MultiGenerator method createLinks', () => {
-	const event = new Event(
-		'Meet with friends',
-		new Date(Date.UTC(2021, 5, 18, 15, 0)),
-		new Date(Date.UTC(2021, 5, 18, 17, 0)),
-		"Let's grab some coffee!",
-		'Blue Bottle Coffee, 300 S Broadway, Los Angeles',
-		[
-			['jane@example.com', 'Jane'],
-			['fred@example.com', 'Fred']
-		]
-	);
 	const generators = {
 		outlook: new Outlook(),
 		office365: new Office365(),
@@ -22,30 +11,72 @@ describe('class MultiGenerator method createLinks', () => {
 	};
 	const multiGenerator = new MultiGenerator(generators);
 
-	it('should generate correct links for each provider', () => {
-		const links = multiGenerator.createLinks(event);
+	it('should generate correct links for each provider w/ minimum info', () => {
+		const event = new Event('Meet with friends', new Date(Date.UTC(2021, 5, 18, 15, 0)));
+		const linksObject = multiGenerator.createLinks(event);
+		const linksArray = multiGenerator.createLinks(event, true);
 
-		expect(links.outlook).toEqual(
-			"https://outlook.live.com/calendar/0/deeplink/compose?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&subject=Meet%20with%20friends&startdt=2021-06-18T15%3A00%3A00Z&enddt=2021-06-18T17%3A00%3A00Z&body=Let's%20grab%20some%20coffee!&location=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&to=Jane%3Cjane%40example.com%3E%2CFred%3Cfred%40example.com%3E"
+		const outlookLink =
+			'https://outlook.live.com/calendar/0/deeplink/compose?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&subject=Meet%20with%20friends&startdt=2021-06-18&enddt=2021-06-19&allday=true';
+		const office365Link =
+			'https://outlook.office.com/calendar/0/deeplink/compose?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&subject=Meet%20with%20friends&startdt=2021-06-18&enddt=2021-06-19&allday=true';
+		const googleLink =
+			'https://calendar.google.com/calendar/render?action=TEMPLATE&text=Meet%20with%20friends&dates=20210618%2F20210619';
+		const yahooLink =
+			'https://calendar.yahoo.com/?v=60&title=Meet%20with%20friends&st=20210618&et=20210619';
+		const icsData =
+			'data:text/calendar;charset=utf8,BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nDTSTART:2021-06-18\nDTEND:2021-06-19\nSUMMARY:Meet%20with%20friends\nEND:VEVENT\nEND:VCALENDAR';
+
+		expect(linksObject.outlook).toEqual(outlookLink);
+		expect(linksObject.office365).toEqual(office365Link);
+		expect(linksObject.google).toEqual(googleLink);
+		expect(linksObject.yahoo).toEqual(yahooLink);
+		expect(linksObject.ics).toEqual(icsData);
+
+		expect(linksArray).toEqual([
+			['outlook', outlookLink],
+			['office365', office365Link],
+			['google', googleLink],
+			['yahoo', yahooLink],
+			['ics', icsData]
+		]);
+	});
+
+	it('should generate correct links for each provider w/ maximum info', () => {
+		const event = new Event(
+			'Meet with friends',
+			new Date(Date.UTC(2021, 5, 18, 15, 0)),
+			new Date(Date.UTC(2021, 5, 18, 17, 0)),
+			"Let's grab some coffee!",
+			'Blue Bottle Coffee, 300 S Broadway, Los Angeles',
+			[['jane@example.com', 'Jane'], ['fred@example.com'], 'joe@example.com']
 		);
-		expect(links.office365).toEqual(
-			"https://outlook.office.com/calendar/0/deeplink/compose?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&subject=Meet%20with%20friends&startdt=2021-06-18T15%3A00%3A00Z&enddt=2021-06-18T17%3A00%3A00Z&body=Let's%20grab%20some%20coffee!&location=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&to=Jane%3Cjane%40example.com%3E%2CFred%3Cfred%40example.com%3E"
-		);
-		expect(links.google).toEqual(
-			"https://calendar.google.com/calendar/render?action=TEMPLATE&text=Meet%20with%20friends&dates=20210618T150000Z%2F20210618T170000Z&details=Let's%20grab%20some%20coffee!&location=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&add=Jane%3Cjane%40example.com%3E%2CFred%3Cfred%40example.com%3E"
-		);
-		expect(links.yahoo).toEqual(
-			"https://calendar.yahoo.com/?v=60&title=Meet%20with%20friends&st=20210618T150000Z&et=20210618T170000Z&desc=Let's%20grab%20some%20coffee!&in_loc=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&inv_list=Jane%3Cjane%40example.com%3E%2CFred%3Cfred%40example.com%3E"
-		);
-		expect(links.ics).toEqual(`data:text/calendar;charset=utf8,BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-DTSTART:2021-06-18T15%3A00%3A00Z
-DTEND:2021-06-18T17%3A00%3A00Z
-SUMMARY:Meet%20with%20friends
-DESCRIPTION:Let's%20grab%20some%20coffee!
-LOCATION:Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles
-END:VEVENT
-END:VCALENDAR`);
+		const linksObject = multiGenerator.createLinks(event);
+		const linksArray = multiGenerator.createLinks(event, true);
+
+		const outlookLink =
+			"https://outlook.live.com/calendar/0/deeplink/compose?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&subject=Meet%20with%20friends&startdt=2021-06-18T15%3A00%3A00Z&enddt=2021-06-18T17%3A00%3A00Z&body=Let's%20grab%20some%20coffee!&location=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&to=Jane%3Cjane%40example.com%3E%2Cfred%40example.com%2Cjoe%40example.com";
+		const office365Link =
+			"https://outlook.office.com/calendar/0/deeplink/compose?path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&subject=Meet%20with%20friends&startdt=2021-06-18T15%3A00%3A00Z&enddt=2021-06-18T17%3A00%3A00Z&body=Let's%20grab%20some%20coffee!&location=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&to=Jane%3Cjane%40example.com%3E%2Cfred%40example.com%2Cjoe%40example.com";
+		const googleLink =
+			"https://calendar.google.com/calendar/render?action=TEMPLATE&text=Meet%20with%20friends&dates=20210618T150000Z%2F20210618T170000Z&details=Let's%20grab%20some%20coffee!&location=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&add=Jane%3Cjane%40example.com%3E%2Cfred%40example.com%2Cjoe%40example.com";
+		const yahooLink =
+			"https://calendar.yahoo.com/?v=60&title=Meet%20with%20friends&st=20210618T150000Z&et=20210618T170000Z&desc=Let's%20grab%20some%20coffee!&in_loc=Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles&inv_list=Jane%3Cjane%40example.com%3E%2Cfred%40example.com%2Cjoe%40example.com";
+		const icsData =
+			"data:text/calendar;charset=utf8,BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nDTSTART:2021-06-18T15%3A00%3A00Z\nDTEND:2021-06-18T17%3A00%3A00Z\nSUMMARY:Meet%20with%20friends\nDESCRIPTION:Let's%20grab%20some%20coffee!\nLOCATION:Blue%20Bottle%20Coffee%2C%20300%20S%20Broadway%2C%20Los%20Angeles\nEND:VEVENT\nEND:VCALENDAR";
+
+		expect(linksObject.outlook).toEqual(outlookLink);
+		expect(linksObject.office365).toEqual(office365Link);
+		expect(linksObject.google).toEqual(googleLink);
+		expect(linksObject.yahoo).toEqual(yahooLink);
+		expect(linksObject.ics).toEqual(icsData);
+
+		expect(linksArray).toEqual([
+			['outlook', outlookLink],
+			['office365', office365Link],
+			['google', googleLink],
+			['yahoo', yahooLink],
+			['ics', icsData]
+		]);
 	});
 });
