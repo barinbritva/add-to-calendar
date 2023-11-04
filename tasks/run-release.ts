ee83d65e0482f514bdbd96d7ef22533b49ff2f7f
Barin Britva <barinbritva@outlook.com>
2023-11-04 20:45:56 +0400

    Replace build scripts with CLI commands.

--- tasks/run-release.ts
+++
@@ -1,8 +0,0 @@
-import {finishWithFail, finishWithSuccess} from './process';
-import {release} from './release';
-
-try {
-	release().then(finishWithSuccess.bind(null, 'Successfully released.')).catch(finishWithFail);
-} catch (error) {
-	finishWithFail(error);
-}
